8ef0810cb92a98d5d5d1966fbac68b8802c94fbc
Hunter Wilhelm <71348224+hunterwilhelm@users.noreply.github.com>
2026-09-09 17:22:13 -0700

    removed unnecessary changes

--- dev/integration_tests/android_hardware_smoke_test/lib/constants.dart
+++ dev/integration_tests/android_hardware_smoke_test/lib/constants.dart
@@ -7,7 +7,8 @@
 // =============================================================================
 
 /// The MethodChannel name used for query and control of native platform capabilities.
-const nativeSupportChannelName = 'com.example.android_hardware_smoke_test/native_support';
+const nativeSupportChannelName =
+    'com.example.android_hardware_smoke_test/native_support';
 
 /// The MethodChannel method name used to query the active graphics rendering backend.
 const methodImpellerBackend = 'impeller_backend';
@@ -124,7 +125,8 @@ const platformViewPrefix = 'platformView';
 const kPlatformViewTextureLayerTest = '${platformViewPrefix}TextureLayerTest';
 
 /// Scenario name for embedding a native platform view using Hybrid Composition.
-const kPlatformViewHybridCompositionTest = '${platformViewPrefix}HybridCompositionTest';
+const kPlatformViewHybridCompositionTest =
+    '${platformViewPrefix}HybridCompositionTest';
 
 /// Scenario name for embedding a native platform view using Hybrid Composition++.
 const kPlatformViewHybridCompositionPlusPlusTest =
